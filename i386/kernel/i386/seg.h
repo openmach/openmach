1f9ecb04db109fae55de336fdf6aa02c0675208a
Shawn Debnath <sd@openmach.org>
2014-08-17 15:21:26 -0700

    GDT fixes for boot setup
    1. Removed padding from descriptor used to load GDT
    2. Added gcc attribute packed for the descriptor
    3. Clobbered memory for all load inline assembly

--- i386/kernel/i386/seg.h
+++ i386/kernel/i386/seg.h
@@ -44,68 +44,66 @@
  * Real segment descriptor.
  */
 struct real_descriptor {
-	unsigned int	limit_low:16,	/* limit 0..15 */
-			base_low:16,	/* base  0..15 */
-			base_med:8,	/* base  16..23 */
-			access:8,	/* access byte */
-			limit_high:4,	/* limit 16..19 */
-			granularity:4,	/* granularity */
-			base_high:8;	/* base 24..31 */
+    unsigned int    limit_low:16,   /* limit 0..15 */
+                    base_low:16,    /* base  0..15 */
+                    base_med:8,     /* base  16..23 */
+                    access:8,       /* access byte */
+                    limit_high:4,   /* limit 16..19 */
+                    granularity:4,  /* granularity */
+                    base_high:8;    /* base 24..31 */
 };
 
 struct real_gate {
-	unsigned int	offset_low:16,	/* offset 0..15 */
-			selector:16,
-			word_count:8,
-			access:8,
-			offset_high:16;	/* offset 16..31 */
+	unsigned int    offset_low:16,	/* offset 0..15 */
+                    selector:16,
+                    word_count:8,
+                    access:8,
+                    offset_high:16; /* offset 16..31 */
 };
 
 #endif !ASSEMBLER
 
-#define	SZ_32		0x4			/* 32-bit segment */
-#define SZ_16		0x0			/* 16-bit segment */
-#define	SZ_G		0x8			/* 4K limit field */
+#define	SZ_32		    0x4	    /* 32-bit segment */
+#define SZ_16           0x0     /* 16-bit segment */
+#define	SZ_G		    0x8		/* 4K limit field */
 
-#define	ACC_A		0x01			/* accessed */
-#define	ACC_TYPE	0x1e			/* type field: */
+#define	ACC_A		    0x01	/* accessed */
+#define	ACC_TYPE	    0x1e	/* type field: */
 
-#define	ACC_TYPE_SYSTEM	0x00			/* system descriptors: */
+#define	ACC_TYPE_SYSTEM	0x00	/* system descriptors: */
 
-#define	ACC_LDT		0x02			    /* LDT */
-#define	ACC_CALL_GATE_16 0x04			    /* 16-bit call gate */
-#define	ACC_TASK_GATE	0x05			    /* task gate */
-#define	ACC_TSS		0x09			    /* task segment */
-#define	ACC_CALL_GATE	0x0c			    /* call gate */
-#define	ACC_INTR_GATE	0x0e			    /* interrupt gate */
-#define	ACC_TRAP_GATE	0x0f			    /* trap gate */
+#define	ACC_LDT		    0x02	/* LDT */
+#define	ACC_CALL_GATE_16 0x04	/* 16-bit call gate */
+#define	ACC_TASK_GATE	0x05    /* task gate */
+#define	ACC_TSS		    0x09	    /* task segment */
+#define	ACC_CALL_GATE	0x0c    /* call gate */
+#define	ACC_INTR_GATE	0x0e    /* interrupt gate */
+#define	ACC_TRAP_GATE	0x0f    /* trap gate */
 
-#define	ACC_TSS_BUSY	0x02			    /* task busy */
+#define	ACC_TSS_BUSY	0x02    /* task busy */
 
-#define	ACC_TYPE_USER	0x10			/* user descriptors */
+#define	ACC_TYPE_USER	0x10	/* user descriptors */
 
-#define	ACC_DATA	0x10			    /* data */
-#define	ACC_DATA_W	0x12			    /* data, writable */
-#define	ACC_DATA_E	0x14			    /* data, expand-down */
-#define	ACC_DATA_EW	0x16			    /* data, expand-down,
-							     writable */
-#define	ACC_CODE	0x18			    /* code */
-#define	ACC_CODE_R	0x1a			    /* code, readable */
-#define	ACC_CODE_C	0x1c			    /* code, conforming */
-#define	ACC_CODE_CR	0x1e			    /* code, conforming,
-						       readable */
-#define	ACC_PL		0x60			/* access rights: */
-#define	ACC_PL_K	0x00			/* kernel access only */
-#define	ACC_PL_U	0x60			/* user access */
-#define	ACC_P		0x80			/* segment present */
+#define	ACC_DATA        0x10	/* data */
+#define	ACC_DATA_W      0x12    /* data, writable */
+#define	ACC_DATA_E      0x14    /* data, expand-down */
+#define	ACC_DATA_EW     0x16    /* data, expand-down, writable */
+#define	ACC_CODE        0x18    /* code */
+#define	ACC_CODE_R      0x1a    /* code, readable */
+#define	ACC_CODE_C      0x1c    /* code, conforming */
+#define	ACC_CODE_CR     0x1e    /* code, conforming, readable */
+#define	ACC_PL          0x60	/* access rights: */
+#define	ACC_PL_K        0x00	/* kernel access only */
+#define	ACC_PL_U        0x60	/* user access */
+#define	ACC_P           0x80	/* segment present */
 
 /*
  * Components of a selector
  */
-#define	SEL_LDT		0x04			/* local selector */
-#define	SEL_PL		0x03			/* privilege level: */
-#define	SEL_PL_K	0x00			    /* kernel selector */
-#define	SEL_PL_U	0x03			    /* user selector */
+#define	SEL_LDT		0x04        /* local selector */
+#define	SEL_PL		0x03		/* privilege level: */
+#define	SEL_PL_K	0x00		/* kernel selector */
+#define	SEL_PL_U	0x03		/* user selector */
 
 /*
  * Convert selector to descriptor table index.
@@ -121,24 +119,23 @@ struct real_gate {
 /* Format of a "pseudo-descriptor", used for loading the IDT and GDT.  */
 struct pseudo_descriptor
 {
-	short pad;
 	unsigned short limit;
 	unsigned long linear_base;
-};
+} __attribute__((packed));
 
 
 /* Load the processor's IDT, GDT, or LDT pointers.  */
 MACH_INLINE void lgdt(struct pseudo_descriptor *pdesc)
 {
-	__asm volatile("lgdt %0" : : "m" (pdesc->limit));
+	__asm volatile("lgdt %0" : : "m" (*pdesc) : "memory");
 }
 MACH_INLINE void lidt(struct pseudo_descriptor *pdesc)
 {
-	__asm volatile("lidt %0" : : "m" (pdesc->limit));
+	__asm volatile("lidt %0" : : "m" (*pdesc) : "memory");
 }
 MACH_INLINE void lldt(unsigned short ldt_selector)
 {
-	__asm volatile("lldt %w0" : : "r" (ldt_selector));
+	__asm volatile("lldt %w0" : : "r" (ldt_selector) : "memory");
 }
 
 #ifdef CODE16
